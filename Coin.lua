4faad1641d9c4789ecd8473270bec19269d573e3
Steve Warren <steve@interactivenyc.com>
2016-04-26 11:39:45 -0400

    fixing drag

--- Coin.lua
+++ Coin.lua
@@ -3,6 +3,7 @@ Coin = class(Sprite)
 function Coin:init(x,y,w,h,r,imgName)
     -- you can accept and set parameters here
     self:setupSprite(x,y,w,h,r,imgName)
+    self.dragging = false
 end
 
 --function Coin:draw()
--- Sprite.lua
+++ Sprite.lua
@@ -13,7 +13,9 @@ function Sprite:setupSprite(x,y,w,h,r,imgName)
     self.r = r
     self.imgName = imgName
     self.draggable = false
-    self.dragging = true
+    --self.dragging = true
+    self.touchStart = vec2(0,0)
+    self.dragDelta = vec2(0,0)
 end
 
 function Sprite:setDraggable(val)
@@ -28,28 +30,34 @@ function Sprite:draw()
     rotate(self.r)
     sprite("Project:"..self.imgName,0,0,self.w,self.h)
     text("r:"..self.r,0,-self.h/2 - 10)
+    if self.dragging then
+        local p = vec2(self.dragDelta[1],self.dragDelta[2])
+        p = p:rotate(-self.r)
+        sprite("Project:"..self.imgName,p[1],p[2],self.w,self.h)
+    end
     popMatrix()
 end
 
 function Sprite:touched(touch)
     -- Codea does not automatically call this method
     if hitTest(touch.x,touch.y,self.x,self.y,self.w,self.h) then
-
         if touch.state == BEGAN then
             print("touch began "..self.imgName)
             if self.draggable then
                 self.dragging = true
-            end
-        elseif touch.state == MOVING then
-            if self.draggable then
-                print("drag: "..self.imgName.."("..touch.x..","..touch.y..")")
-                sprite("Project:"..self.imgName,touch.x,touch.y,self.w,self.h)
-            end
-        elseif touch.state == ENDED then
-            print("touch ended "..self.imgName)
-            if self.draggable then
-                self.dragging = false
+                self.touchStart = vec2(touch.x,touch.y)
             end
         end
     end
+    if self.dragging then
+        if touch.state == MOVING then
+                print("drag: "..self.imgName.."("..touch.x..","..touch.y..")")
+                self.dragDelta = vec2(touch.x-self.touchStart[1],touch.y-self.touchStart[2])
+        elseif touch.state == ENDED then
+            print("touch ended "..self.imgName)
+            self.dragging = false
+            self.dragDelta = vec2(0,0)
+        end
+    end
+    
 end
